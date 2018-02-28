47bb93cd7175b3ff4fb8419fe8982e9bfe4f4097
Brad Kolonay <bmkolonay@gmail.com>
2018-02-28 10:43:26 -0800

    Remove unneeded css and html elements. Add function that returns dummy data

--- zignet.ui/src/App.js
+++ zignet.ui/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  getLatestSuiteResults() {
+    return {
+      totalPassedTests: 10,
+      totalFailedTests: 25
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title">ZigNet</h1>
       </div>
     );
   }
