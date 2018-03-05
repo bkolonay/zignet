0146d945d7bab6fb021b7b40e136a869d6b6c554
Brad Kolonay <bmkolonay@gmail.com>
2018-03-04 16:59:22 -0800

    Add empty unit test project

---
+++ zignet.adapter/ZigNet.Adapter.SpecFlow.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZigNet.Adapter.SpecFlow.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
