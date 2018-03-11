3b43413eba765f3d11bde10dfdd76e29cd417546
Brad Kolonay <bmkolonay@gmail.com>
2018-03-11 12:17:41 -0700

    Reduce usage of this.props

--- zignet.ui/src/home/SuiteResultChart.js
+++ zignet.ui/src/home/SuiteResultChart.js
@@ -6,26 +6,27 @@ import ChartistPieChart from '../common/ChartistPieChart';
 
 class SuiteResultChart extends Component {
 
-  _getChartData() {
+  _getChartData(suiteResult) {
     return {
       series: [
-        this.props.suiteResult.TotalPassedTests,
-        this.props.suiteResult.TotalFailedTests
+        suiteResult.TotalPassedTests,
+        suiteResult.TotalFailedTests
       ]
     };
   }
 
-  _getTotalTests() {
-    return this.props.suiteResult.TotalPassedTests + this.props.suiteResult.TotalFailedTests;
+  _getTotalTests(suiteResult) {
+    return suiteResult.TotalPassedTests + suiteResult.TotalFailedTests;
   }
 
   render() {
+    const suiteResult = this.props.suiteResult;
     return (
       <div className="col-4">
-        <h3 className="text-center">{this.props.suiteResult.SuiteName}</h3>
-        <ChartistPieChart chartId={this.props.suiteResult.SuiteID}
-                          chartData={this._getChartData()} />
-        <p className="text-center"><Link to={`/suiteResult/${this.props.suiteResult.SuiteID}`}>Total: {this._getTotalTests()}</Link></p>
+        <h3 className="text-center">{suiteResult.SuiteName}</h3>
+        <ChartistPieChart chartId={suiteResult.SuiteID}
+                          chartData={this._getChartData(suiteResult)} />
+        <p className="text-center"><Link to={`/suiteResult/${suiteResult.SuiteID}`}>Total: {this._getTotalTests(suiteResult)}</Link></p>
       </div>
     );
   }
