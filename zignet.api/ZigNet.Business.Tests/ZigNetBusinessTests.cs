c3588b6250e86fb254118e209f1aab0d2162f627
Brad Kolonay <bmkolonay@gmail.com>
2018-03-26 13:52:37 -0700

    Uncomment unit tests that caused build issues in the past

--- zignet.api/ZigNet.Business.Tests/ZigNetBusinessTests.cs
+++ zignet.api/ZigNet.Business.Tests/ZigNetBusinessTests.cs
@@ -7,6 +7,7 @@ using ZigNet.Domain.Suite;
 using System.Collections.Generic;
 using ZigNet.Domain.Test;
 using ZigNetTestResult = ZigNet.Domain.Test.TestResult;
+using ZigNet.Database.DTOs;
 
 namespace ZigNet.Business.Tests
 {
@@ -15,148 +16,148 @@ namespace ZigNet.Business.Tests
         [TestClass]
         public class GetLatestSuiteResultsMethod
         {
-            //[TestMethod]
-            //public void ReturnsLatestSuiteResultsFromOneSuite()
-            //{
-            //    var zignetDatabaseMock = new Mock<IZigNetDatabase>();
-            //    var suite = new Suite { SuiteID = 1, Name = "Suite 1" };
-            //    var suites = new List<Suite> { suite };
-            //    var suiteResults = new List<SuiteResult> {
-            //        new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:00am"), EndTime = DateTime.Parse("12/19/2017 10:05am")},
-            //        new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:02am"), EndTime = DateTime.Parse("12/19/2017 10:10am") },
-            //        new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:01am"), EndTime = DateTime.Parse("12/19/2017 10:03am") }
-            //    };
-            //    var testResults = new List<ZigNetTestResult>{
-            //        new ZigNetTestResult { ResultType = TestResultType.Fail },
-            //        new ZigNetTestResult { ResultType = TestResultType.Inconclusive },
-            //        new ZigNetTestResult { ResultType = TestResultType.Pass },
-            //        new ZigNetTestResult { ResultType = TestResultType.Pass },
-            //    };
+            [TestMethod]
+            public void ReturnsLatestSuiteResultsFromOneSuite()
+            {
+                var zignetDatabaseMock = new Mock<IZigNetDatabase>();
+                var suite = new Suite { SuiteID = 1, Name = "Suite 1" };
+                var suites = new List<Suite> { suite };
+                var suiteResults = new List<SuiteResult> {
+                    new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:00am"), EndTime = DateTime.Parse("12/19/2017 10:05am")},
+                    new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:02am"), EndTime = DateTime.Parse("12/19/2017 10:10am") },
+                    new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:01am"), EndTime = DateTime.Parse("12/19/2017 10:03am") }
+                };
+                var testResults = new List<ZigNetTestResult>{
+                    new ZigNetTestResult { ResultType = TestResultType.Fail },
+                    new ZigNetTestResult { ResultType = TestResultType.Inconclusive },
+                    new ZigNetTestResult { ResultType = TestResultType.Pass },
+                    new ZigNetTestResult { ResultType = TestResultType.Pass },
+                };
 
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetSuites()).Returns(suites);
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetSuiteResultsForSuite(It.IsAny<int>())).Returns(suiteResults);
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetTestResultsForSuiteResult(It.IsAny<int>())).Returns(testResults);
+                zignetDatabaseMock.Setup(zdm => zdm.GetSuites()).Returns(suites);
+                zignetDatabaseMock.Setup(zdm => zdm.GetSuiteResultsForSuite(It.IsAny<int>())).Returns(suiteResults);
+                zignetDatabaseMock.Setup(zdm => zdm.GetTestResultsForSuiteResult(It.IsAny<int>())).Returns(testResults);
 
-            //    var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
+                var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
 
-            //    var suiteSummaries = zigNetBusiness.GetLatestSuiteResults().ToList();
-            //    var suiteSummary = suiteSummaries[0];
+                var suiteSummaries = zigNetBusiness.GetLatestSuiteResults().ToList();
+                var suiteSummary = suiteSummaries[0];
 
-            //    Assert.AreEqual(1, suiteSummaries.Count());
-            //    Assert.AreEqual(1, suiteSummary.SuiteID);
-            //    Assert.AreEqual("Suite 1", suiteSummary.SuiteName);
-            //    Assert.AreEqual(1, suiteSummary.TotalFailedTests);
-            //    Assert.AreEqual(1, suiteSummary.TotalInconclusiveTests);
-            //    Assert.AreEqual(2, suiteSummary.TotalPassedTests);
-            //    Assert.AreEqual(DateTime.Parse("12/19/2017 10:10am"), suiteSummary.SuiteEndTime);
-            //}
+                Assert.AreEqual(1, suiteSummaries.Count());
+                Assert.AreEqual(1, suiteSummary.SuiteID);
+                Assert.AreEqual("Suite 1", suiteSummary.SuiteName);
+                Assert.AreEqual(1, suiteSummary.TotalFailedTests);
+                Assert.AreEqual(1, suiteSummary.TotalInconclusiveTests);
+                Assert.AreEqual(2, suiteSummary.TotalPassedTests);
+                Assert.AreEqual(DateTime.Parse("12/19/2017 10:10am"), suiteSummary.SuiteEndTime);
+            }
 
-            //[TestMethod]
-            //public void DoesNotThrowWhenNoSuiteResultsForSuite()
-            //{
-            //    var zignetDatabaseMock = new Mock<IZigNetDatabase>();
-            //    var suite = new Suite { SuiteID = 1, Name = "Suite 1" };
-            //    var suites = new List<Suite> { suite };
-            //    var suiteResults = new List<SuiteResult> { };
-            //    var testResults = new List<ZigNetTestResult>{
-            //        new ZigNetTestResult { ResultType = TestResultType.Fail },
-            //        new ZigNetTestResult { ResultType = TestResultType.Inconclusive },
-            //        new ZigNetTestResult { ResultType = TestResultType.Pass },
-            //        new ZigNetTestResult { ResultType = TestResultType.Pass },
-            //    };
+            [TestMethod]
+            public void DoesNotThrowWhenNoSuiteResultsForSuite()
+            {
+                var zignetDatabaseMock = new Mock<IZigNetDatabase>();
+                var suite = new Suite { SuiteID = 1, Name = "Suite 1" };
+                var suites = new List<Suite> { suite };
+                var suiteResults = new List<SuiteResult> { };
+                var testResults = new List<ZigNetTestResult>{
+                    new ZigNetTestResult { ResultType = TestResultType.Fail },
+                    new ZigNetTestResult { ResultType = TestResultType.Inconclusive },
+                    new ZigNetTestResult { ResultType = TestResultType.Pass },
+                    new ZigNetTestResult { ResultType = TestResultType.Pass },
+                };
 
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetSuites()).Returns(suites);
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetSuiteResultsForSuite(It.IsAny<int>())).Returns(suiteResults);
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetTestResultsForSuiteResult(It.IsAny<int>())).Returns(testResults);
+                zignetDatabaseMock.Setup(zdm => zdm.GetSuites()).Returns(suites);
+                zignetDatabaseMock.Setup(zdm => zdm.GetSuiteResultsForSuite(It.IsAny<int>())).Returns(suiteResults);
+                zignetDatabaseMock.Setup(zdm => zdm.GetTestResultsForSuiteResult(It.IsAny<int>())).Returns(testResults);
 
-            //    var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
+                var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
 
-            //    var suiteSummaries = zigNetBusiness.GetLatestSuiteResults().ToList();
+                var suiteSummaries = zigNetBusiness.GetLatestSuiteResults().ToList();
 
-            //    Assert.AreEqual(0, suiteSummaries.Count());
-            //}
+                Assert.AreEqual(0, suiteSummaries.Count());
+            }
 
-            //[TestMethod]
-            //public void DoesNotThrowWhenSuiteEndTimeNull()
-            //{
-            //    var zignetDatabaseMock = new Mock<IZigNetDatabase>();
-            //    var suite = new Suite { SuiteID = 1, Name = "Suite 1" };
-            //    var suites = new List<Suite> { suite };
-            //    var suiteResults = new List<SuiteResult> {
-            //        new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:00am"), EndTime = DateTime.Parse("12/19/2017 10:05am")},
-            //        new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:02am") },
-            //        new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:01am"), EndTime = DateTime.Parse("12/19/2017 10:03am") }
-            //    };
-            //    var testResults = new List<ZigNetTestResult>{
-            //        new ZigNetTestResult { ResultType = TestResultType.Fail },
-            //        new ZigNetTestResult { ResultType = TestResultType.Inconclusive },
-            //        new ZigNetTestResult { ResultType = TestResultType.Pass },
-            //        new ZigNetTestResult { ResultType = TestResultType.Pass },
-            //    };
+            [TestMethod]
+            public void DoesNotThrowWhenSuiteEndTimeNull()
+            {
+                var zignetDatabaseMock = new Mock<IZigNetDatabase>();
+                var suite = new Suite { SuiteID = 1, Name = "Suite 1" };
+                var suites = new List<Suite> { suite };
+                var suiteResults = new List<SuiteResult> {
+                    new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:00am"), EndTime = DateTime.Parse("12/19/2017 10:05am")},
+                    new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:02am") },
+                    new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:01am"), EndTime = DateTime.Parse("12/19/2017 10:03am") }
+                };
+                var testResults = new List<ZigNetTestResult>{
+                    new ZigNetTestResult { ResultType = TestResultType.Fail },
+                    new ZigNetTestResult { ResultType = TestResultType.Inconclusive },
+                    new ZigNetTestResult { ResultType = TestResultType.Pass },
+                    new ZigNetTestResult { ResultType = TestResultType.Pass },
+                };
 
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetSuites()).Returns(suites);
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetSuiteResultsForSuite(It.IsAny<int>())).Returns(suiteResults);
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetTestResultsForSuiteResult(It.IsAny<int>())).Returns(testResults);
+                zignetDatabaseMock.Setup(zdm => zdm.GetSuites()).Returns(suites);
+                zignetDatabaseMock.Setup(zdm => zdm.GetSuiteResultsForSuite(It.IsAny<int>())).Returns(suiteResults);
+                zignetDatabaseMock.Setup(zdm => zdm.GetTestResultsForSuiteResult(It.IsAny<int>())).Returns(testResults);
 
-            //    var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
+                var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
 
-            //    var suiteSummaries = zigNetBusiness.GetLatestSuiteResults().ToList();
-            //    var suiteSummary = suiteSummaries[0];
+                var suiteSummaries = zigNetBusiness.GetLatestSuiteResults().ToList();
+                var suiteSummary = suiteSummaries[0];
 
-            //    Assert.AreEqual(1, suiteSummaries.Count());
-            //    Assert.AreEqual(1, suiteSummary.SuiteID);
-            //    Assert.AreEqual("Suite 1", suiteSummary.SuiteName);
-            //    Assert.AreEqual(1, suiteSummary.TotalFailedTests);
-            //    Assert.AreEqual(1, suiteSummary.TotalInconclusiveTests);
-            //    Assert.AreEqual(2, suiteSummary.TotalPassedTests);
-            //    Assert.IsNull(suiteSummary.SuiteEndTime);
-            //}
+                Assert.AreEqual(1, suiteSummaries.Count());
+                Assert.AreEqual(1, suiteSummary.SuiteID);
+                Assert.AreEqual("Suite 1", suiteSummary.SuiteName);
+                Assert.AreEqual(1, suiteSummary.TotalFailedTests);
+                Assert.AreEqual(1, suiteSummary.TotalInconclusiveTests);
+                Assert.AreEqual(2, suiteSummary.TotalPassedTests);
+                Assert.IsNull(suiteSummary.SuiteEndTime);
+            }
 
-            //[TestMethod]
-            //public void DoesNotThrowWhenNoSuites()
-            //{
-            //    var zignetDatabaseMock = new Mock<IZigNetDatabase>();
-            //    var suites = new List<Suite> { };
+            [TestMethod]
+            public void DoesNotThrowWhenNoSuites()
+            {
+                var zignetDatabaseMock = new Mock<IZigNetDatabase>();
+                var suites = new List<Suite> { };
 
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetSuites()).Returns(suites);
+                zignetDatabaseMock.Setup(zdm => zdm.GetSuites()).Returns(suites);
 
-            //    var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
+                var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
 
-            //    var suiteSummaries = zigNetBusiness.GetLatestSuiteResults().ToList();
+                var suiteSummaries = zigNetBusiness.GetLatestSuiteResults().ToList();
 
-            //    Assert.AreEqual(0, suiteSummaries.Count());
-            //}
+                Assert.AreEqual(0, suiteSummaries.Count());
+            }
 
-            //[TestMethod]
-            //public void DoesNotThrowWhenNoTestResultsForSuiteResult()
-            //{
-            //    var zignetDatabaseMock = new Mock<IZigNetDatabase>();
-            //    var suite = new Suite { SuiteID = 1, Name = "Suite 1" };
-            //    var suites = new List<Suite> { suite };
-            //    var suiteResults = new List<SuiteResult> {
-            //        new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:00am"), EndTime = DateTime.Parse("12/19/2017 10:05am")},
-            //        new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:02am"), EndTime = DateTime.Parse("12/19/2017 10:10am") },
-            //        new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:01am"), EndTime = DateTime.Parse("12/19/2017 10:03am") }
-            //    };
-            //    var testResults = new List<ZigNetTestResult> { };
+            [TestMethod]
+            public void DoesNotThrowWhenNoTestResultsForSuiteResult()
+            {
+                var zignetDatabaseMock = new Mock<IZigNetDatabase>();
+                var suite = new Suite { SuiteID = 1, Name = "Suite 1" };
+                var suites = new List<Suite> { suite };
+                var suiteResults = new List<SuiteResult> {
+                    new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:00am"), EndTime = DateTime.Parse("12/19/2017 10:05am")},
+                    new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:02am"), EndTime = DateTime.Parse("12/19/2017 10:10am") },
+                    new SuiteResult { Suite = suite, StartTime = DateTime.Parse("12/19/2017 10:01am"), EndTime = DateTime.Parse("12/19/2017 10:03am") }
+                };
+                var testResults = new List<ZigNetTestResult> { };
 
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetSuites()).Returns(suites);
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetSuiteResultsForSuite(It.IsAny<int>())).Returns(suiteResults);
-            //    zignetDatabaseMock.Setup(zdm => zdm.GetTestResultsForSuiteResult(It.IsAny<int>())).Returns(testResults);
+                zignetDatabaseMock.Setup(zdm => zdm.GetSuites()).Returns(suites);
+                zignetDatabaseMock.Setup(zdm => zdm.GetSuiteResultsForSuite(It.IsAny<int>())).Returns(suiteResults);
+                zignetDatabaseMock.Setup(zdm => zdm.GetTestResultsForSuiteResult(It.IsAny<int>())).Returns(testResults);
 
-            //    var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
+                var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
 
-            //    var suiteSummaries = zigNetBusiness.GetLatestSuiteResults().ToList();
-            //    var suiteSummary = suiteSummaries[0];
+                var suiteSummaries = zigNetBusiness.GetLatestSuiteResults().ToList();
+                var suiteSummary = suiteSummaries[0];
 
-            //    Assert.AreEqual(1, suiteSummaries.Count());
-            //    Assert.AreEqual(1, suiteSummary.SuiteID);
-            //    Assert.AreEqual("Suite 1", suiteSummary.SuiteName);
-            //    Assert.AreEqual(0, suiteSummary.TotalFailedTests);
-            //    Assert.AreEqual(0, suiteSummary.TotalInconclusiveTests);
-            //    Assert.AreEqual(0, suiteSummary.TotalPassedTests);
-            //    Assert.AreEqual(DateTime.Parse("12/19/2017 10:10am"), suiteSummary.SuiteEndTime);
-            //}
+                Assert.AreEqual(1, suiteSummaries.Count());
+                Assert.AreEqual(1, suiteSummary.SuiteID);
+                Assert.AreEqual("Suite 1", suiteSummary.SuiteName);
+                Assert.AreEqual(0, suiteSummary.TotalFailedTests);
+                Assert.AreEqual(0, suiteSummary.TotalInconclusiveTests);
+                Assert.AreEqual(0, suiteSummary.TotalPassedTests);
+                Assert.AreEqual(DateTime.Parse("12/19/2017 10:10am"), suiteSummary.SuiteEndTime);
+            }
         }
 
         [TestClass]
@@ -489,6 +490,41 @@ namespace ZigNet.Business.Tests
         [TestClass]
         public class GetLatestTestResultsMethod
         {
+            [TestMethod]
+            public void MapsLatestTestResults()
+            {
+                var utcNow = DateTime.UtcNow;
+
+                var zignetDatabaseMock = new Mock<IZigNetDatabase>();
+                zignetDatabaseMock.Setup(zdm => zdm.GetLatestTestResults(1)).Returns(
+                    new List<LatestTestResult>{ 
+                        new LatestTestResult { TestName = "test1", FailingFromDate = utcNow, TestResultID = 2 } 
+                    }
+                );
+
+                var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
+                var latestTestResults = zigNetBusiness.GetLatestTestResults(1).ToList();
+
+                Assert.AreEqual(1, latestTestResults.Count);
+                Assert.AreEqual("test1", latestTestResults[0].TestName);
+                Assert.AreEqual(utcNow, latestTestResults[0].FailingFromDate);
+                Assert.AreEqual(2, latestTestResults[0].TestResultID);
+                Assert.IsNull(latestTestResults[0].PassingFromDate);
+            }
+
+            [TestMethod]
+            public void DoesNotThrowWhenZeroLatestTestResults()
+            {
+                var utcNow = DateTime.UtcNow;
+
+                var zignetDatabaseMock = new Mock<IZigNetDatabase>();
+                zignetDatabaseMock.Setup(zdm => zdm.GetLatestTestResults(1)).Returns(new List<LatestTestResult>());
+                var zigNetBusiness = new ZigNetBusiness(zignetDatabaseMock.Object);
+                var latestTestResults = zigNetBusiness.GetLatestTestResults(1).ToList();
+
+                Assert.AreEqual(0, latestTestResults.Count);
+            }
+
             [TestMethod]
             public void ReturnsFirstTimeTestPassedWhenTestHasAlwaysPassed()
             {
