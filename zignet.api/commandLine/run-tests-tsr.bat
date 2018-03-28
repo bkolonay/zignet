616b68ecaf8cc1dc96cadcf37c706a5b629c4344
Brad <bmkolonay@gmail.com>
2018-03-28 10:16:03 -0700

    Add batch files that run service tests via specrun command line

---
+++ zignet.api/commandLine/run-tests-tsr.bat
@@ -0,0 +1,2 @@
+cd C:\service-automation
+C:\service-automation\SpecRun.Runner.1.2.0\tools\SpecRun.exe run Service_Endpoints.srprofile /basefolder:"C:\service-automation\Release-tsr" /outputfolder:specrun-output
