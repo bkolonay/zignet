b1b7831abf6c2f8dc896459ecf67eefc3eac2f1e
Brad <bmkolonay@gmail.com>
2018-03-28 15:33:06 -0700

    Add temporary sorting that returns debug suite names after release ones

--- zignet.api/ZigNet.Api/Controllers/LatestSuiteResultsController.cs
+++ zignet.api/ZigNet.Api/Controllers/LatestSuiteResultsController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using ZigNet.Business;
 using ZigNet.Database.DTOs;
@@ -16,7 +17,11 @@ namespace ZigNet.Api.Controllers
 
         public IEnumerable<SuiteSummary> Get()
         {
-            return _zigNetBusiness.GetLatestSuiteResults();
+            var latestSuiteResults = _zigNetBusiness.GetLatestSuiteResults();
+            var debugSuiteResults = latestSuiteResults.Where(sr => sr.SuiteName.Contains("(D)"));
+            var releaseSuiteResults = latestSuiteResults.Where(sr => !sr.SuiteName.Contains("(D)")).ToList();
+            releaseSuiteResults.AddRange(debugSuiteResults);
+            return releaseSuiteResults;
         }
     }
 }
