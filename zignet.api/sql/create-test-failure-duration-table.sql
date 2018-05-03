4ea206333d41539ee5f617f3dcae314430dec7bc
Brad <bmkolonay@gmail.com>
2018-05-03 14:42:30 -0700

    Add test failure duration table creation script

---
+++ zignet.api/sql/create-test-failure-duration-table.sql
@@ -0,0 +1,22 @@
+--ALTER TABLE SuiteResults
+--DROP CONSTRAINT FK_SuiteResultSuiteResultType
+
+----DROP TABLE IF EXISTS SuiteResultTypes
+--DROP TABLE SuiteResults
+
+CREATE TABLE TestFailureDurations (
+	TestFailureDurationID int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	SuiteId int NOT NULL,
+	TestId int NOT NULL,
+	TestResultId int NOT NULL,
+	FailureStartDateTime datetime,
+	FailureEndDateTime datetime,
+	CONSTRAINT FK_TestFailureDurationSuite FOREIGN KEY (SuiteId)
+	REFERENCES Suites(SuiteID),
+	CONSTRAINT FK_TestFailureDurationTest FOREIGN KEY (TestId)
+	REFERENCES Tests(TestID),
+	CONSTRAINT FK_TestFailureDurationTestResult FOREIGN KEY (TestResultId)
+	REFERENCES TestResults(TestResultID)
+)
+
+SELECT * FROM TestFailureDurations
