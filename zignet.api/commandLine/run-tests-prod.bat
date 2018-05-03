5443d35c83bd5a981851544c8fb928081509e042
Brad <bmkolonay@gmail.com>
2018-05-03 11:20:16 -0700

    Add batch file that runs prod tests from cmd

---
+++ zignet.api/commandLine/run-tests-prod.bat
@@ -0,0 +1,3 @@
+#spec-run profile needs to have @ProdSafe tag set
+cd C:\service-automation
+C:\service-automation\SpecRun.Runner.1.2.0\tools\SpecRun.exe run Service_Endpoints.srprofile /basefolder:"C:\service-automation\Release-prod" /outputfolder:specrun-output
