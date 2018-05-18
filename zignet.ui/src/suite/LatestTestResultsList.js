e23314f611f7a369a89ca874f72f9854114e965b
Brad <bmkolonay@gmail.com>
2018-05-18 15:45:22 -0700

    Center suite name on list page

--- zignet.ui/src/suite/LatestTestResultsList.js
+++ zignet.ui/src/suite/LatestTestResultsList.js
@@ -29,10 +29,15 @@ class LatestTestResultsList extends Component {
     return (
       <div className="container">
         <h4>{this._getHomeLink(this.props.testResultsGrouped)}</h4>
-        <h2 className="text-center">
-          <span>{this.props.suiteName}</span>
-          {this._getGroupButton()}
-        </h2>
+        <div className="row">
+          <div className="col-4"/>
+          <div className="col-4">
+            <h2 className="text-center">{this.props.suiteName}</h2>
+          </div>
+          <div className="col-4">
+            {this._getGroupButton()}
+          </div>
+        </div>
         <table className="table">
           <thead>
             <tr>
