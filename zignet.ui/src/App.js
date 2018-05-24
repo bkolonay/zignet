a6ed17754592f0fc127a13a6b12f5f76a473fe37
Brad <bmkolonay@gmail.com>
2018-05-24 09:48:47 -0700

    Move creation of api class to app instead of passing from index

--- zignet.ui/src/App.js
+++ zignet.ui/src/App.js
@@ -3,19 +3,17 @@ import { Route, Switch } from 'react-router-dom'
 import LatestSuiteResults from './home/LatestSuiteResults'
 import LatestTestResults from './suite/LatestTestResults'
 import BadRoute from './common/BadRoute'
+import ZigNetApi from './api/ZigNetApi'
 
 class App extends Component {
 
   render() {
+    let zigNetApi = new ZigNetApi(process.env.REACT_APP_API_BASE_URL + 'api/');
     return (
       <div>
         <Switch>
-          <Route exact path="/" 
-                 render={(props) => <LatestSuiteResults {...props} zigNetApi={this.props.zigNetApi} />} 
-          />
-          <Route path='/:suiteId'
-                 render={(props) => <LatestTestResults {...props} zigNetApi={this.props.zigNetApi} />}
-          />
+          <Route exact path="/" render={(props) => <LatestSuiteResults {...props} zigNetApi={zigNetApi} />} />
+          <Route path='/:suiteId' render={(props) => <LatestTestResults {...props} zigNetApi={zigNetApi} />} />
           <Route component={BadRoute} />
         </Switch>
       </div>
--- zignet.ui/src/index.js
+++ zignet.ui/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
-import ZigNetApi from './api/ZigNetApi'
 import App from './App';
 import './common/bootstrap.css'
 
 ReactDOM.render((
   	<BrowserRouter>
-      <App zigNetApi={new ZigNetApi(process.env.REACT_APP_API_BASE_URL + 'api/')} />
+      <App />
   	</BrowserRouter>
   ), document.getElementById('root')
 );
