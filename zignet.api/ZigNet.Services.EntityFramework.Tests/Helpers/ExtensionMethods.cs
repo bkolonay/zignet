cebfe29b83af0c38b3bc90a8cbf9243616ac0b9e
Brad <bmkolonay@gmail.com>
2018-08-07 09:27:00 -0700

    Add unit test project for Services.EF project. Add tests for deleting temporary test results

---
+++ zignet.api/ZigNet.Services.EntityFramework.Tests/Helpers/ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ZigNet.Services.EntityFramework.Tests.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static Mock<DbSet<T>> ToDbSetMock<T>(this IEnumerable<T> list) where T : class
+        {
+            var listAsQueryable = list.AsQueryable();
+            var dbSetMock = new Mock<DbSet<T>>();
+
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.Provider).Returns(listAsQueryable.Provider);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.Expression).Returns(listAsQueryable.Expression);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(listAsQueryable.ElementType);
+            dbSetMock.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(listAsQueryable.GetEnumerator());
+
+            return dbSetMock;
+
+            // code taken from:
+            // https://www.jankowskimichal.pl/en/2016/01/mocking-dbcontext-and-dbset-with-moq/
+            // https://msdn.microsoft.com/en-us/library/dn314429(v=vs.113).aspx
+        }
+    }
+}
---
+++ zignet.api/ZigNet.Services.EntityFramework.Tests/TemporaryTestResultsServiceTests.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using ZigNet.Database.EntityFramework;
+using ZigNet.Services.EntityFramework.Tests.Helpers;
+
+namespace ZigNet.Services.EntityFramework.Tests
+{
+    class TemporaryTestResultsServiceTests
+    {
+        [TestClass]
+        public class DeleteAllWithSuiteIdMethod
+        {
+            [TestMethod]
+            public void DeletesSingle()
+            {
+                var temporaryTestResults = new List<TemporaryTestResult>
+                {
+                    new TemporaryTestResult { SuiteId = 1 }
+                };
+                var mockDbSet = temporaryTestResults.ToDbSetMock();
+
+                var mockContext = new Mock<ZigNetEntities>();
+                mockContext.Setup(m => m.TemporaryTestResults).Returns(mockDbSet.Object);
+
+                var zignetEntitiesWrapperMock = new Mock<IZigNetEntitiesWrapper>();
+                zignetEntitiesWrapperMock.Setup(z => z.Get()).Returns(mockContext.Object);
+
+                var temporaryTestResultsService = new TemporaryTestResultsService(zignetEntitiesWrapperMock.Object);
+                temporaryTestResultsService.DeleteAll(1);
+
+                mockDbSet.Verify(d => d.RemoveRange(temporaryTestResults), Times.Once);
+            }
+
+            [TestMethod]
+            public void DeletesMultiple()
+            {
+                var temporaryTestResults = new List<TemporaryTestResult>
+                {
+                    new TemporaryTestResult { SuiteId = 1 },
+                    new TemporaryTestResult { SuiteId = 1 }
+                };
+                var mockDbSet = temporaryTestResults.ToDbSetMock();
+
+                var mockContext = new Mock<ZigNetEntities>();
+                mockContext.Setup(m => m.TemporaryTestResults).Returns(mockDbSet.Object);
+
+                var zignetEntitiesWrapperMock = new Mock<IZigNetEntitiesWrapper>();
+                zignetEntitiesWrapperMock.Setup(z => z.Get()).Returns(mockContext.Object);
+
+                var temporaryTestResultsService = new TemporaryTestResultsService(zignetEntitiesWrapperMock.Object);
+                temporaryTestResultsService.DeleteAll(1);
+
+                mockDbSet.Verify(d => d.RemoveRange(temporaryTestResults), Times.Once);
+            }
+
+            [TestMethod]
+            public void DeletesOnlyWithSuiteId()
+            {
+                var temporaryTestResults = new List<TemporaryTestResult>
+                {
+                    new TemporaryTestResult { SuiteId = 1 },
+                    new TemporaryTestResult { SuiteId = 1 },
+                    new TemporaryTestResult { SuiteId = 2 }
+                };
+                var mockDbSet = temporaryTestResults.ToDbSetMock();
+
+                var mockContext = new Mock<ZigNetEntities>();
+                mockContext.Setup(m => m.TemporaryTestResults).Returns(mockDbSet.Object);
+
+                var zignetEntitiesWrapperMock = new Mock<IZigNetEntitiesWrapper>();
+                zignetEntitiesWrapperMock.Setup(z => z.Get()).Returns(mockContext.Object);
+
+                var temporaryTestResultsService = new TemporaryTestResultsService(zignetEntitiesWrapperMock.Object);
+                temporaryTestResultsService.DeleteAll(1);
+
+                var temporaryTestResultsExpectedToBeDeleted = temporaryTestResults.Where(t => t.SuiteId == 1);
+                mockDbSet.Verify(d => d.RemoveRange(temporaryTestResultsExpectedToBeDeleted), Times.Once);
+            }
+
+            [TestMethod]
+            public void DoesNotThrowWhenNoneWithSuiteId()
+            {
+                var temporaryTestResults = new List<TemporaryTestResult>
+                {
+                    new TemporaryTestResult { SuiteId = 2 },
+                    new TemporaryTestResult { SuiteId = 3 }
+                };
+                var mockDbSet = temporaryTestResults.ToDbSetMock();
+
+                var mockContext = new Mock<ZigNetEntities>();
+                mockContext.Setup(m => m.TemporaryTestResults).Returns(mockDbSet.Object);
+
+                var zignetEntitiesWrapperMock = new Mock<IZigNetEntitiesWrapper>();
+                zignetEntitiesWrapperMock.Setup(z => z.Get()).Returns(mockContext.Object);
+
+                var temporaryTestResultsService = new TemporaryTestResultsService(zignetEntitiesWrapperMock.Object);
+                temporaryTestResultsService.DeleteAll(1);
+
+                var temporaryTestResultsExpectedToBeDeleted = new List<TemporaryTestResult>();
+                mockDbSet.Verify(d => d.RemoveRange(temporaryTestResultsExpectedToBeDeleted), Times.Once);
+            }
+        }
+    }
+}
