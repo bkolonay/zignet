6a87dd82cb927a8841538caba6258f14bef9f391
Brad <bmkolonay@gmail.com>
2018-12-19 12:27:04 -0800

    Add event handling of application checkbox filters on home page

--- zignet.ui/src/home/Filter.js
+++ zignet.ui/src/home/Filter.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
 
 class Filter extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			showLoopNet: false,
+			showLmMobile: false,
+		};
+
+		this.handleInputChange = this.handleInputChange.bind(this);
+	}
+
+	handleInputChange(event) {
+		const target = event.target;
+		const value = target.checked;
+		const name = target.name;
+
+		this.setState({
+			[name]: value
+		})
+	}
+
 
   render() {
     return (
       <div>
-		<div className="form-check">
-		  <input className="form-check-input" type="checkbox" value="" id="LoopNet"/>
-		  <label className="form-check-label" htmlFor="LoopNet">LoopNet</label>
-		</div>
-		<div className="form-check">
-		  <input className="form-check-input" type="checkbox" value="" id="LoopNet"/>
-		  <label className="form-check-label" htmlFor="LoopNet">LM Mobile</label>
-		</div>
-		<div className="form-check">
-		  <input className="form-check-input" type="checkbox" value="" id="LoopNet"/>
-		  <label className="form-check-label" htmlFor="LoopNet">CityFeet</label>
-		</div>
-		<div className="form-check">
-		  <input className="form-check-input" type="checkbox" value="" id="LoopNet"/>
-		  <label className="form-check-label" htmlFor="LoopNet">Showcase</label>
-		</div>
+				<div className="form-check">
+				  <input 
+				  	name="showLoopNet"
+				  	type="checkbox" 
+				  	checked={this.state.showLoopNet}
+				  	onChange={this.handleInputChange}
+				  	className="form-check-input"
+				  	id="showLoopNet"/>
+				  <label className="form-check-label" htmlFor="showLoopNet">LoopNet</label>
+				</div>
+				<div className="form-check">
+				  <input 
+				  	name="showLmMobile"
+				  	type="checkbox"
+				  	checked={this.state.showLmMobile}
+				  	onChange={this.handleInputChange}
+				  	className="form-check-input"
+				  	id="showLmMobile"/>
+				  <label className="form-check-label" htmlFor="showLmMobile">LmMobile</label>
+				</div>
       </div>
     );
   }
