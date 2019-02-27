c5102f19c015db8a50cc0a34957644fc545ce3c6
Brad <bmkolonay@gmail.com>
2019-02-27 14:27:41 -0800

    Delete uneeded folder

--- zignet.api/commandLine/run-tests-dvm.bat
+++
@@ -1,2 +0,0 @@
-cd C:\service-automation
-C:\service-automation\SpecRun.Runner.1.2.0\tools\SpecRun.exe run Service_Endpoints.srprofile /basefolder:"C:\service-automation\Release-dvm" /outputfolder:specrun-output
